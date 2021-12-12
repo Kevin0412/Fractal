ecb918792bd25f42c6c5314fc7ca9a383b3a5b87
Kevin0412 <45508696+Kevin0412@users.noreply.github.com>
2021-12-12 20:45:34 +0800

    Add files via upload

---
+++ boat_colour_continue.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+def color(n):
+    if int((n%1530)/255)==0:
+        return(0,n%255,255)
+    if int((n%1530)/255)==1:
+        return(0,255,255-n%255)
+    if int((n%1530)/255)==2:
+        return(n%255,255,0)
+    if int((n%1530)/255)==3:
+        return(255,255-n%255,0)
+    if int((n%1530)/255)==4:
+        return(255,0,n%255)
+    if int((n%1530)/255)==5:
+        return(255-n%255,0,255)
+m=8192
+C=int(1530/17/3)
+c=-1.25+1/(2*m)*1j
+n=0+0j
+t=0
+while(True):
+    if abs(n)<=2:
+        n=n**2+c
+        if abs(n)>2:
+            break
+    t+=1
+t=int((t-1)/C)+1
+print(t)
+#img=np.zeros((4*m,4*m,3),np.uint8)
+#cv2.imwrite('boat_colour_'+str(m)+'_'+str(C)+'_'+str(t)+'.png',img)
+img=cv2.imread('boat_colour_'+str(m)+'_'+str(C)+'_'+str(t)+'.png')
+#print(img[0][0])
+for y in range(0,4*m):
+    print(y)
+    file=open('boat.txt','w+')
+    file.write(str(y))
+    file.close()
+    for x in range(4*m):
+        c=(x+0.5)/m-2+((y+0.5)/m-2)*1j
+        n=0+0j
+        for z in range(C*t):
+            if abs(n)<=2:
+                n=(abs(n.real)+abs(n.imag)*1j)**2+c
+                if abs(n)>2:
+                    img[y][x]=color(int(z*1530/C))
+                    break
+    cv2.imwrite('boat_colour_'+str(m)+'_'+str(C)+'_'+str(t)+'.png',img)
---
+++ boat_colour_continue_2.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+def color(n):
+    if n==-1:
+        return(0,0,0)
+    elif int((n%1530)/255)==0:
+        return(0,n%255,255)
+    elif int((n%1530)/255)==1:
+        return(0,255,255-n%255)
+    elif int((n%1530)/255)==2:
+        return(n%255,255,0)
+    elif int((n%1530)/255)==3:
+        return(255,255-n%255,0)
+    elif int((n%1530)/255)==4:
+        return(255,0,n%255)
+    elif int((n%1530)/255)==5:
+        return(255-n%255,0,255)
+m=8000
+c=-1.25+1/(2*m)*1j
+n=0+0j
+img=np.ones((4*m,4*m,3),np.uint8)*255
+#cv2.imwrite('absimag_colour_'+str(m)+'_'+str(C)+'_'+str(t)+'.png',img)
+#img=cv2.imread('boat_colour_8000.png')
+for y in range(2*m-1,-1,-1):
+    print(y)
+    for x in range(4*m):
+        c=(x+0.5)/m-2+((y+0.5)/m-2)*1j
+        n=0+0j
+        for z in range(1531):
+            if abs(n)<=2:
+                n=(abs(n.real)+abs(n.imag)*1j)**2+c
+                if abs(n)>2:
+                    img[4*m-y-1][x]=color(z-1)
+                    break
+        c=(x+0.5)/m-2-((y+0.5)/m-2)*1j
+        n=0+0j
+        for z in range(1531):
+            if abs(n)<=2:
+                n=(abs(n.real)+abs(n.imag)*1j)**2+c
+                if abs(n)>2:
+                    img[y][x]=color(z-1)
+                    break
+    if y%8==0:
+        cv2.imwrite('boat_colour_'+str(m)+'.png',img)
+        file=open('boat.txt','w+')
+        file.write(str(y-1))
+        file.close()
+cv2.imwrite('baot_colour_'+str(m)+'.png',img)
